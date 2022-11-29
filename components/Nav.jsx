9658c6752d483354bb860ebdc8c435b69d298d47
innocentdavid <paulinnocent04@gmail.com>
2022-11-29 14:34:20 +0100

    initial commit

---
+++ components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div className="flex justify-between items-center text-white px-[130px] text-xs">
+            <div className="">الاسئلة الشائعة</div>
+            <div className="">تشكيل الفرق </div>
+            <div className="">التسجيل</div>
+            <div className="">الجوائز</div>
+            <div className="">ورش العمل</div>
+            <div className="">سير الهاكاثون</div>
+            <div className="">نبذه عن الهاكاثون</div>
+            <div className="">
+                <img src="/images/logo01.png" alt="" width="40px" />
+            </div>
+        </div>
+    )
+}
